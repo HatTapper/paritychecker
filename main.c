84e9c1f823a531f5e0405f279a3714fc5f42bb26
HatTapper <aidanmyles206@gmail.com>
2024-08-01 00:32:42 -0400

    added multithreading for testing

--- main.c
+++ main.c
@@ -13,6 +13,14 @@
 
 #define NOPARITYBIT -1
 
+typedef struct DataPacket
+{
+    int data;
+    int parityBit;
+} DataPacket;
+
+
+
 // returns 1 if bit = 1, returns 0 if bit = 0
 int bitCheck(unsigned int number, unsigned int bitIndex)
 {
@@ -36,13 +44,13 @@ int getParity(unsigned int num)
 // "receive" data and check if it has an even parity
 // if so, return 1 to verify data is correctly received
 // else, return 0 to warn that data is not correctly received
-int dataVerifier(int data[2])
+int dataVerifier(DataPacket* dataPacket)
 {
     // check if data has even parity and parity bit is set to 0
-    if(getParity(data[0]) % 2 == 0 && data[1] == 0)
+    if(getParity(dataPacket->data) % 2 == 0 && dataPacket->parityBit == 0)
         return 1;
     // if data is odd parity, check if parity bit is set to 1
-    else if(data[1] == 1)
+    else if(dataPacket->parityBit == 1)
         return 1;
     // if data is odd parity with the parity bit, return 0
     else
@@ -51,33 +59,54 @@ int dataVerifier(int data[2])
 
 
 // mimics sending of data from a different location by using a separate thread to initialize and transmit data to the original thread
-int* sendData(void)
+void* sendData(void* dataBuffer)
 {
-    int dataToSend[2] = {31, NOPARITYBIT};
-    if(getParity(dataToSend[0] % 2 != 0))
-        dataToSend[1] = 1;
+    ((DataPacket*)dataBuffer)->data = 24;
+    if(getParity(((DataPacket*)dataBuffer)->data % 2 != 0))
+        ((DataPacket*)dataBuffer)->parityBit = 0;
     else 
-        dataToSend[1] = 0;
+        ((DataPacket*)dataBuffer)->parityBit = 1;
 
-    return 31, 0;
+    return NULL;
+}
+
+void* sendBadData(void* dataBuffer)
+{
+    ((DataPacket*)dataBuffer)->data = 2;
+    ((DataPacket*)dataBuffer)->parityBit = 0;
+
+    return NULL;
 }
 
 
 
 int main(void)
 {
-    // ptr to store location of data array
-    int* pdata;
+    // struct to act as a location for data to be sent to
+    DataPacket correctData;
     // create thread to simulate sending of data
     pthread_t threadId;
-    pthread_create(&threadId, NULL, sendData, NULL);
-    pthread_join(threadId, pdata);
-    // dereference data
+    pthread_create(&threadId, NULL, sendData, &correctData);
+    pthread_join(threadId, NULL);
+    
 
-    if(dataVerifier(newData))
+    if(dataVerifier(&correctData))
         printf("Data was correctly received.\n");
     else
         printf("Data was incorrectly received.\n");
+
+    
+    // intentionally incorrect data
+    DataPacket badData;
+
+    pthread_create(&threadId, NULL, sendBadData, &badData);
+    pthread_join(threadId, NULL);
+
+    if(dataVerifier(&badData))
+        printf("Data was correctly received.\n");
+    else
+        printf("Data was incorrectly received.\n");
+
 
     return 0;
 }
